c1f4b1dde3a0c5968304243b750ae550052319bf
shanhong cheng <shnahongcheng@gmail.com>
2016-06-05 09:58:32 -0600

    react is fun

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,7 +21,7 @@ function Header() {
         <Navigation className={s.nav} />
         <Link className={s.brand} to="/">
           <img src={logoUrl} width="38" height="38" alt="React" />
-          <span className={s.brandTxt}>Your Company</span>
+          <span className={s.brandTxt}>React is fun</span>
         </Link>
         <div className={s.banner}>
           <h1 className={s.bannerTitle}>React</h1>
