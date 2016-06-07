73da87dd0ed49a4cc92b82f473ff0bb9a2924e48
shanhong cheng <shnahongcheng@gmail.com>
2016-06-06 23:09:01 -0600

    actions

--- src/routes/games/Sudoku.js
+++ src/routes/games/Sudoku.js
@@ -4,11 +4,44 @@ import s from './Sudoku.css';
 
 class Sudoku extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      showJson: false
+    }
+  }
+
+  startNewGame() {
+    console.log('new game')
+  }
+
+  toggleJsonShow() {
+    console.log('toggle json show');
+    this.setState({
+      showJson: !this.state.showJson
+    })
+  }
+
+  undo() {
+    console.log('undo')
+  }
+
+  solve() {
+    console.log('solve puzzle')
+  }
+
   render() {
     return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>Sudoku</h1>
+      <div className="container">
+        <div className="row">
+          <ul className={s.buttonList}>
+            <li>
+              <a className="waves-effect waves-light btn" onClick={this.startNewGame.bind(this)}>New Game</a>
+            </li>
+            <li><a className="waves-effect waves-light btn" onClick={this.toggleJsonShow.bind(this)}>Toggle JSON</a></li>
+            <li><a className="waves-effect waves-light btn" onClick={this.undo.bind(this)}>Undo</a></li>
+            <li><a className="waves-effect waves-light btn" onClick={this.solve.bind(this)}>solve</a></li>
+          </ul>
         </div>
       </div>
     );
