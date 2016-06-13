f8fbb2373551c5524d861ce3e59d6f0972d816b2
shanhong cheng <shanhong.cheng@utah.edu>
2016-06-13 13:17:10 -0600

    list

--- src/routes/d3/demo.js
+++ src/routes/d3/demo.js
@@ -1,6 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './demo.css';
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+
 class Demo extends Component {
   static contextTypes = {
     setTitle: PropTypes.func.isRequired,
@@ -15,8 +23,30 @@ class Demo extends Component {
   }
 
   render() {
+    let styles = {
+      height: "900px"
+    }
     return (
-      <div className="d3demo" />
+      <div className="HolyGrail-body" style={styles}>
+        <main className="HolyGrail-content">…</main>
+        <nav className="HolyGrail-nav">
+          <List>
+            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
+            <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
+            <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
+            <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
+            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
+          </List>
+          <Divider />
+          <List>
+            <ListItem primaryText="All mail" rightIcon={<ActionInfo />} />
+            <ListItem primaryText="Trash" rightIcon={<ActionInfo />} />
+            <ListItem primaryText="Spam" rightIcon={<ActionInfo />} />
+            <ListItem primaryText="Follow up" rightIcon={<ActionInfo />} />
+          </List>
+        </nav>
+        <aside className="HolyGrail-ads">…</aside>
+      </div>
     )
   }
 }
